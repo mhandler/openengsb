d4d66aa27a59d87362dbe9b5404006b5ff09e8a2
Michael Handler <michael.handler@fullstop.at>
2010-04-23 17:43:38 +0200

    Add interceptor project.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

---
+++ core/ekb/interceptor/src/main/java/org/openengsb/ekb/interceptor/MessageInterceptor.java
@@ -0,0 +1,32 @@
+package org.openengsb.ekb.core.messageinterceptor;
+
+import org.apache.servicemix.nmr.api.Exchange;
+import org.apache.servicemix.nmr.api.event.ExchangeListener;
+import org.apache.servicemix.nmr.api.internal.InternalExchange;
+
+public class MessageInterceptor implements ExchangeListener {
+
+    @Override
+    public void exchangeDelivered(Exchange exchange) {
+        // do nothing
+    }
+
+    @Override
+    public void exchangeFailed(Exchange exchange) {
+        // do nothing
+    }
+
+    @Override
+    public void exchangeSent(Exchange exchange) {
+        if (exchange instanceof InternalExchange) {
+            InternalExchange iex = (InternalExchange) exchange;
+            internalExchangeSent(iex);
+        }
+        System.out.println("MessageInterceptor - Exchange sent - not an internal exchange");
+    }
+
+    private void internalExchangeSent(InternalExchange iex) {
+        System.out.println("MessageInterceptor - Exchange sent - destination id: " + iex.getDestination().getId());
+    }
+
+}
