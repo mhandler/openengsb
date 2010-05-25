9db900d094f024bbe9da339193b83020d3d1ab7b
Michael Handler <michael.handler@fullstop.at>
2010-05-25 15:23:44 +0200

    Final cleanup of old ekb code.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

--- core/ekb/core/src/main/java/org/openengsb/ekb/core/messagetransformation/Transformation.java
+++ core/ekb/core/src/main/java/org/openengsb/ekb/core/messagetransformation/Transformation.java
@@ -1,10 +1,7 @@
 package org.openengsb.ekb.core.messagetransformation;
 
-
 public interface Transformation {
 
     Object transform(Object input) throws TransformationException;
-
-    Class<?> getTargetType(Object input);
 
 }
