c0076d8c36df87cc1aaa64fa5a15c45cb9f21340
Michael Handler <michael.handler@fullstop.at>
2010-05-30 19:10:15 +0200

    First draft of jar reader implementation.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

--- core/ekb/model-analyzer/src/main/java/org/openengsb/ekb/analyzer/JarReader.java
+++ core/ekb/model-analyzer/src/main/java/org/openengsb/ekb/analyzer/JarReader.java
@@ -17,6 +17,63 @@
  */
 package org.openengsb.ekb.analyzer;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.openengsb.ekb.annotations.Concept;
+
 public class JarReader {
+
+    private URI jar;
+
+    private URLClassLoader classloader;
+
+    public JarReader(URI jar) {
+        this.jar = jar;
+        try {
+            this.classloader = new URLClassLoader(new URL[] { jar.toURL() });
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Class<?>> readInConcepts() throws IOException {
+        List<Class<?>> concepts = new ArrayList<Class<?>>();
+        JarFile jarFile = new JarFile(new File(jar));
+        Enumeration<JarEntry> entries = jarFile.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry jarEntry = entries.nextElement();
+            if (jarEntry.getName().endsWith(".class")) {
+                Class<?> clazz = loadClass(jarEntry.getName());
+                if (isConcept(clazz)) {
+                    concepts.add(clazz);
+                }
+            }
+        }
+        return concepts;
+    }
+
+    private boolean isConcept(Class<?> clazz) {
+        return clazz.getAnnotation(Concept.class) != null;
+    }
+
+    private Class<?> loadClass(String entry) throws IOException {
+        try {
+            String className = entry.replaceAll("/", ".");
+            className = className.substring(0, className.lastIndexOf('.'));
+            return classloader.loadClass(className);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
